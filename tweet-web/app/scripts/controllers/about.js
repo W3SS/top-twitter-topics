f92ac4e71262f98499541edcdf02639cd8b58e44
Anderson Farias <anderson.faryas@gmail.com>
2016-05-19 20:13:22 -0300

    Creation of the web front-web app

---
+++ tweet-web/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tweetWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tweetWebApp
+ */
+angular.module('tweetWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ tweet-web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tweetWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tweetWebApp
+ */
+angular.module('tweetWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
